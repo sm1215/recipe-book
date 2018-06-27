525725357fa83ab49ddd3d6671a06d271dd91a57
sm1215 <sm1215@gmail.com>
2018-06-27 14:50:33 -0400

    updated ingredients and added image assets

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,12 +4,20 @@ import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styles: [`
+    .ingredient-thumb {
+      height:20px;
+      width:20px;
+      margin:0 5px 0 0;
+    }
+  `]
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
+    new Ingredient('Apple', 5, '../assets/images/ingredients/apple.png'),
+    new Ingredient('Hylian Shroom', 10, '../assets/images/ingredients/hylian-shroom.png'),
+    new Ingredient('Raw Bird Drumstick', 2, '../assets/images/ingredients/raw-bird-drumstick.png'),
+    new Ingredient('Raw Meat', 7, '../assets/images/ingredients/raw-meat.png')
   ];
 
   constructor() { }
